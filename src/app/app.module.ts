732fbd6a125d6a5bb9f2f83ff77034008d1c1093
Lucas <lucasterra02@gmail.com>
2019-01-11 17:10:35 -0200

    lucas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { CategoriaService } from '../services/domain/categoria.service';
+import { ErrorInterceptorsProvider } from '../interceptors/error-interceptors';
+import { AuthService } from '../services/auth.service';
 
 @NgModule({
   declarations: [
-    MyApp,
-    HomePage
+    MyApp
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    HomePage
+    MyApp
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    CategoriaService,
+    ErrorInterceptorsProvider,
+    AuthService
   ]
 })
 export class AppModule {}
