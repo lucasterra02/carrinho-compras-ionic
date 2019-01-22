497ab902d9daed316b69447289c868c38ba06b1e
Lucas <lucasterra02@gmail.com>
2019-01-22 17:51:42 -0200

    form group

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -28,12 +28,24 @@ export class HomePage {
     this.menu.swipeEnable(true);
   }
 
+  ionViewDidEnter() {
+    this.auth.refreshToken().subscribe(response => {
+      this.auth.successfulLogin(response.headers.get('Authorization'));
+      this.navCtrl.setRoot('CategoriasPage');
+    },
+      error => { });
+  }
+
   login() {
     this.auth.authenticate(this.creds).subscribe(response => {
       this.auth.successfulLogin(response.headers.get('Authorization'));
       this.navCtrl.setRoot('CategoriasPage');
     },
       error => { });
+  }
+
+  signup() {
+    this.navCtrl.push('SignupPage');
   }
 
 }
