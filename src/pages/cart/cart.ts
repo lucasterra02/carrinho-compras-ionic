96502c2abdec9154a3d1eb943e0da5b69d2a3e33
Lucas Terra <lucasterra02@gmail.com>
2019-01-28 17:55:03 -0200

    criando carrinho

---
+++ src/pages/cart/cart.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { CartItem } from '../../models/cart-item';
+import { StorageService } from '../../services/storage.service';
+import { CartService } from '../../services/domain/cart.service';
+
+/**
+ * Generated class for the CartPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-cart',
+  templateUrl: 'cart.html',
+})
+export class CartPage {
+
+  items: CartItem[];
+
+  constructor(public navCtrl: NavController, 
+              public navParams: NavParams,
+              public cartService: CartService) {
+  }
+
+  ionViewDidLoad() {
+    let cart = this.cartService.getCart();
+    this.items = cart.items;
+  }
+
+}
